955c59b055f6e182da5251f112c2e4db4af19228
unknown <albertkantwi@gmail.com>
2023-12-20 04:00:48 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_024440) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "greetings", force: :cascade do |t|
-    t.string "text"
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
