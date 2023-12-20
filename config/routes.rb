6fbb472a149d3b0319d0696f20a58f7e844a4dd1
unknown <albertkantwi@gmail.com>
2023-12-20 04:01:52 +0000

    Add all functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'root#index'
-
-  namespace :api do
-    namespace :v1 do
-      resources :greetings, only: [:index]
-    end
-  end
-end
+   get "up" => "rails/health#show", as: :rails_health_check
+   namespace :api, default: {format: :json} do
+   namespace  :v1 do
+     resources :messages, only:[:index] 
+     end
+   end
+ end
